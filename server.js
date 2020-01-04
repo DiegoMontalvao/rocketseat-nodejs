0ff74f62a2eca174cfdcd9917eda495469670024
Diego Montalvao <caals19@gmail.com>
2020-01-04 13:09:36 -0400

    separando os codigos em aquivos separados conforme a arquitetura mvc

--- server.js
+++ server.js
@@ -17,20 +17,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi',
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+app.use('/api', require('./src/routes'));
 
 // ouvir a portar 3000
 
 app.listen(3000);
-
-// criando a primeira rota get
-
-app.get('/', (req, resp) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-    return resp.send('Hello Diego');
-});
