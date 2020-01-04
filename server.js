2854623211836b7f29704dee0ce3efdc1fa2d148
Diego Montalvao <caals19@gmail.com>
2020-01-04 12:46:28 -0400

    criação do model Product

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const requireDir = require ('require-dir');
+
 
 // iniciando o app
 const app = express();
 
 // iniciando o DB
 
-mongoose.connect('mongodb://locahost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+    { useUnifiedTopology: true,
+    useNewUrlParser: true }
+);
+
+// importando o model products para a aplicacao server.js
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// ouvir a portar 3000
 
 app.listen(3000);
 
+// criando a primeira rota get
+
 app.get('/', (req, resp) => {
-    resp.send('Hello Diego');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return resp.send('Hello Diego');
 });
